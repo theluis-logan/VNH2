9eca46617c5c0579074e401b96d6787efdb86a90
fpt-theluis <logan25092001@gmail.com>
2023-07-02 00:22:40 +0700

    first upload

---
+++ src/main/java/DBContext/DBContext.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DBContext;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author NguyenTheAnh
+ */
+public class DBContext {
+    public static Connection makeConnection() throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String URL = "jdbc:sqlserver://localhost:1433;databaseName=VNH2";
+        conn = DriverManager.getConnection(URL, "sa", "12345");
+        return conn;
+    }
+    
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        System.out.println(makeConnection());
+    }
+}
---
+++ src/main/java/com/VNH/user/cotroller/LoginController.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.VNH.user.cotroller;
+
+import com.VNH.user.registration.userDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author thanh
+ */
+@WebServlet(name = "LoginController", urlPatterns = {"/LoginController"})
+public class LoginController extends HttpServlet {
+
+    private final String homePage = "Home.jsp";
+    private final String invalidPage = "invalid.html";
+    
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+           String email = request.getParameter("txtEmail");
+           String password = request.getParameter("txtPassword");
+            userDAO dao = new userDAO();
+            boolean result = dao.checkLogin(email, password);
+            
+            String url = invalidPage;
+            
+            if(result){
+                    url = homePage;
+                    Cookie cookie = new Cookie(email, password);
+                    cookie.setMaxAge(60*3);
+                    response.addCookie(cookie);
+                }
+            request.getRequestDispatcher(url).forward(request, response);
+        }catch(NamingException ex){
+            ex.printStackTrace();
+        } catch(SQLException ex){
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        finally{
+          out.close();
+        }
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
+
